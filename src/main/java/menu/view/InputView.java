4fc49c98c935b19d6268cccad33b5a7fa96d23b2
Min <binine2@gmail.com>
2024-11-26 13:29:11 +0900

    feat(view): 코치 이름 입력 기능 구현

---
+++ src/main/java/menu/view/InputView.java
@@ -0,0 +1,9 @@
+package menu.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static void requestCoachName() {
+        String input = Console.readLine();
+    }
+}
---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,11 @@
+package menu.view;
+
+public class OutputView {
+    public static void printMenuRecommendStart() {
+        System.out.println("점심 메뉴 추천을 시작합니다.");
+    }
+
+    public static void printRequestCoachName() {
+        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+    }
+}
